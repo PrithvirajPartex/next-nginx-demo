d4f64a9e3eecb0621078f7aad1ffa3f936e64cbf
PrithvirajPartex <prithviraj.gorule@partex.ai>
2025-10-08 16:11:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const dev = false;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+// Use PORT from environment variable or fallback to 3000
+const port = process.env.PORT || 3000;
+
 app.prepare().then(() => {
-  createServer((req, res) => handle(req, res)).listen(3000, () => {
-    console.log('Next.js server running on port 3000');
+  createServer((req, res) => handle(req, res)).listen(port, () => {
+    console.log(`Next.js server running on port ${port}`);
   });
 });
